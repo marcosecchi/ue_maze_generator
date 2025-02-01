5b0d625a3819e88a5e58f92ba0949e4eab8e7268
marcosecchi <loziosecchi@gmail.com>
2025-02-01 20:15:51 +0100

    Add maze funtion library.

---
+++ Plugins/MazeGenerator/Source/MazeGenerator/Private/MazeGeneratorFunctionLibrary.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MazeGeneratorFunctionLibrary.h"
+
+TMap<FIntVector, int32> UMazeGeneratorFunctionLibrary::GenerateEmptyMaze(const int32 Width, const int32 Height)
+{
+	TMap<FIntVector, int32> Map;
+	for (int32 X = 0; X < Width; X++)
+	{
+		for (int32 Y = 0; Y < Height; Y++)
+		{
+			Map.Add(FIntVector(X, Y, 0), -1);
+		}
+	}
+
+	return Map;
+}
+
+TMap<FIntVector, int32> UMazeGeneratorFunctionLibrary::GenerateMaze(const int32 Width, const int32 Height)
+{
+	TMap<FIntVector, int32> Map = GenerateEmptyMaze(Width, Height);
+
+	// TODO
+	
+	return Map;
+}
---
+++ Plugins/MazeGenerator/Source/MazeGenerator/Public/MazeGeneratorFunctionLibrary.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MazeGeneratorFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAZEGENERATOR_API UMazeGeneratorFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Maze Generation")
+	static TMap<FIntVector, int32> GenerateEmptyMaze(int32 Width, int32 Height);
+
+	UFUNCTION(BlueprintCallable, Category="Maze Generation")
+	static TMap<FIntVector, int32> GenerateMaze(int32 Width, int32 Height);
+};
